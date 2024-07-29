c9cf28b80e5777513bfc8da3ef2c1dcadcbb4c03
Jonathan Spivey <dze6@txstate.edu>
2024-07-29 18:57:56 -0500

    Added isInCheck() and isInCheckmate() to the board class, but need to get it to work with the movePiece() on Board class

--- board/Board.java
+++ board/Board.java
@@ -11,6 +11,8 @@ import board.pieces.*;
 public class Board {
    private Piece[][] chessBoard;
    private List<Piece> capturedPieces;
+   private int[] whiteKingPosition;
+   private int[] blackKingPosition;
 
    /**
     * Creates an empty board and calls the board initializer.
@@ -23,6 +25,7 @@ public class Board {
    
    /**
     * Initializes the board with all pieces in their starting location.
+    * Initializes the king positions
     */
    private void initializeChessBoard() {
       
@@ -31,6 +34,7 @@ public class Board {
       chessBoard[0][2] = new Bishop("black", 0, 2);
       chessBoard[0][3] = new Queen("black", 0, 3);
       chessBoard[0][4] = new King("black", 0, 4);
+      blackKingPosition = new int[]{0, 4};
       chessBoard[0][5] = new Bishop("black", 0, 5);
       chessBoard[0][6] = new Knight("black", 0, 6);
       chessBoard[0][7] = new Rook("black", 0, 7);
@@ -44,6 +48,7 @@ public class Board {
       chessBoard[7][2] = new Bishop("white", 7, 2);
       chessBoard[7][3] = new Queen("white", 7, 3);
       chessBoard[7][4] = new King("white", 7, 4);
+      whiteKingPosition = new int[]{7, 4};
       chessBoard[7][5] = new Bishop("white", 7, 5);
       chessBoard[7][6] = new Knight("white", 7, 6);
       chessBoard[7][7] = new Rook("white", 7, 7);
@@ -55,35 +60,36 @@ public class Board {
 
    /**
     * Prints the current state of the chessboard to the console.
+    * MAY NEED TO BE DELETED BEFORE SUBMITION
     */
 
-   public void printChessBoard() {
-      System.out.println("  A  B  C  D  E  F  G  H");
-      for (int i = 0; i < 8; i++){
-         System.out.print((8 - i) + " ");
-         for (int j = 0; j < 8; j++){
-            Piece boardSpace = chessBoard[i][j];
-            if (boardSpace != null) { 
-               System.out.print(boardSpace.getIcon() + " ");
-            } else {
-               if ((i + j) % 2 == 1) {
-                  System.out.print("## ");
-               } else {
-                  System.out.print("   ");
-               }
-            }
-         }
-         System.out.println();
-      }
-      System.out.println();
-   }
+   // public void printChessBoard() {
+   //    System.out.println("  A  B  C  D  E  F  G  H");
+   //    for (int i = 0; i < 8; i++){
+   //       System.out.print((8 - i) + " ");
+   //       for (int j = 0; j < 8; j++){
+   //          Piece boardSpace = chessBoard[i][j];
+   //          if (boardSpace != null) { 
+   //             System.out.print(boardSpace.getIcon() + " ");
+   //          } else {
+   //             if ((i + j) % 2 == 1) {
+   //                System.out.print("## ");
+   //             } else {
+   //                System.out.print("   ");
+   //             }
+   //          }
+   //       }
+   //       System.out.println();
+   //    }
+   //    System.out.println();
+   // }
    
    public static boolean isInBounds(int row, int column) {
       return row >= 0 && row < 8 && column >= 0 && column < 8;
    }
 
    /**
-    * Helper method to move pieces. Uses 
+    * Helper method to move pieces. And updates the new positions of the King instances.
     * @param int[] array indicating columns and rows
     * @return true if move was valid and completed, false otherwise.
     */
@@ -99,7 +105,21 @@ public class Board {
          if (moveToSpace != null && !moveToSpace.getColor().equals(pieceToMove.getColor())) {
             addCapturedPiece(moveToSpace);
          }      
-         return pieceToMove.movePiece(chessBoard, toRow, toColumn);
+         
+         boolean moveSuccessful = pieceToMove.movePiece(chessBoard, toRow, toColumn);//calls to move
+         if (moveSuccessful) {
+            if (pieceToMove instanceof King) {
+               if (pieceToMove.getColor().equals("white")) {
+                  whiteKingPosition[0] = toRow;
+                  whiteKingPosition[1] = toColumn;
+              } else {
+                  blackKingPosition[0] = toRow;
+                  blackKingPosition[1] = toColumn;
+               }
+            }
+         }
+         
+         return moveSuccessful;
       }
       return false;
    }
@@ -133,5 +153,101 @@ public class Board {
     */
    public List<Piece> getCapturedPieces() {
       return capturedPieces;
+   }
+
+   /**
+    * Returns the position[row][column] of the king piece given the color
+    * @param kingColor
+    * @return an int[] with the position of the king.
+    */
+   public int[] getKingPosition(String kingColor) {
+      return kingColor.equals("white") ? whiteKingPosition : blackKingPosition;
+   }
+   
+   /**
+    * Checks of the king, given the color of the king is in check from opponent pieces.
+    *
+    * @param kingColor
+    * @return true if the king is in check, false otherwise.
+    */
+   public boolean isInCheck(String kingColor) {
+      int[] kingSpace = kingColor.equals("white") ? whiteKingPosition : blackKingPosition;
+      for (int i = 0; i < 8; i++) {
+         for (int j = 0; j < 8; j++) {
+            Piece checkPiece = chessBoard[i][j];
+            if (checkPiece != null && !checkPiece.getColor().equals(kingColor)) {
+               List<int[]> possibleMoves = checkPiece.possibleMoves(chessBoard);
+               for  (int[] move : possibleMoves) {
+                  if (move[0] == kingSpace[0] && move[1] == kingSpace[1]) {
+                     return true;
+                  }
+               }
+            }
+         }
+      }
+      return false;
+   }
+   
+   /**
+    * Checks if a king is in check mate status, based on if it's in check, if it can move, or if
+    * another piece can block/capture the opposing piece.  
+    *
+    * @param kingColor
+    * @return false if it's not check, can be moved, or can be blocked, true otherwise.
+    */
+   public boolean isInCheckMate(String kingColor) {
+      int[] kingSpace = kingColor.equals("white") ? whiteKingPosition : blackKingPosition;
+      
+      // Checks if the king is in check
+      if (!isInCheck(kingColor)) {
+         return false;
+      }
+      
+      // Checks if the king can move out of the check
+      Piece kingPiece = chessBoard[kingSpace[0]][kingSpace[1]];
+      List<int[]> kingMoves = kingPiece.possibleMoves(chessBoard);
+      for (int[] move : kingMoves) {
+         int toRow = move[0];
+         int toColumn = move[1];
+         Piece placeHolder = chessBoard[toRow][toColumn];
+         
+         chessBoard[toRow][toColumn] = kingPiece; 
+         chessBoard[kingSpace[0]][kingSpace[1]] = null;
+         boolean possibleEscape = !isInCheck(kingColor);
+         
+         chessBoard[kingSpace[0]][kingSpace[1]] = kingPiece;
+         chessBoard[toRow][toColumn] = placeHolder;
+
+         if (possibleEscape) { 
+            return false;
+         }
+      }
+      
+      // Checks if any pieces can block/capture to save the king
+      for (int i = 0; i < 8; i++) {
+         for (int j = 0; j < 8; j++) {
+            Piece checkPiece = chessBoard[i][j];
+            if (checkPiece != null && checkPiece.getColor().equals(kingColor)) {
+               List<int[]> possibleMoves = checkPiece.possibleMoves(chessBoard);
+               for (int[] move : possibleMoves) {
+                  int toRow = move[0];
+                  int toColumn = move[1];
+                  Piece placeHolder = chessBoard[toRow][toColumn];
+
+                  chessBoard[toRow][toColumn] = checkPiece; 
+                  chessBoard[i][j] = null;
+                  boolean possibleBlock = !isInCheck(kingColor);
+
+                  chessBoard[i][j] = checkPiece;
+                  chessBoard[toRow][toColumn] = placeHolder;
+
+                  if (possibleBlock) {
+                     return false;
+                  }
+               }
+            }
+         }
+      }
+      return true; //all conditions for a checkmate were false
    }
 }
